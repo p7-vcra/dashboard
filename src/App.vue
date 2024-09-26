5c84fc833183eb5d814809c5b7316b8c19523053
Gustas Jucaitis <gustasjucaitis@gmail.com>
2024-09-27 00:30:57 +0200

    Lortet virker ikke igen. Aner ikke hvorfor. Sikkert en eller anden module der fucker eller noget i den stil. GL HF

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MapComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import MapComponent from './components/MapComponent.vue'; // Ensure the path is correct
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    MapComponent, // Register the component
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
