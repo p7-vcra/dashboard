bf98526d86166ee72055b7fa9afb2649df02989a
Gustas Jucaitis <gustasjucaitis@gmail.com>
2024-09-27 00:30:57 +0200

    Lortet virker ikke igen. Aner ikke hvorfor. Sikkert en eller anden module der fucker eller noget i den stil. GL HF

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="map-container">
+      <l-map ref="map" v-model:zoom="zoom" :center="[56.27526584557727, 9.321697552292468]">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
+  </template>
+  
+  <script>
+  import "leaflet/dist/leaflet.css";
+  import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+  
+  export default {
+    components: {
+      LMap,
+      LTileLayer,
+    },
+    data() {
+      return {
+        zoom: 2,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .map-container {
+    height: calc(100vh - 50px); /* Full height minus 50px for footer or bottom space */
+    width: 100vw; /* Full width */
+    display: flex;
+  }
+  
+  .l-map {
+    flex: 1; /* Make the map fill all available space */
+  }
+  </style>
+  
