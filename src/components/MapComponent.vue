ae5925c4abfa8e52a2d158df4a88d6f54aa436be
Bergulff <Bergulff@hotmail.com>
2024-09-27 11:40:59 +0200

    new imports at main.ts, and made som changes to the MapComponent.vue. The map works now

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="map-container">
-      <l-map ref="map" v-model:zoom="zoom" :center="[56.27526584557727, 9.321697552292468]">
+      <l-map ref="map" v-model:zoom="zoom" :center="[56.27526584557727, 9.321697552292468]":maxBounds="maxBounds">
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
@@ -21,9 +21,21 @@
     },
     data() {
       return {
-        zoom: 2,
+        zoom: 2.8,
+        maxBounds:[
+          [83.6341, -141.0],
+          [-80.0, 200.0]
+        ],
       };
     },
+    mounted(){
+      if(!this.$refs.map_leaflet_id){
+        console.log("Map is initialized");
+      }
+      else{
+        console.log("Map is already initialized");
+      }
+    }
   };
   </script>
   
