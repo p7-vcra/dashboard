2ea22d30c9df1b73bb6182866e21edf483ab1bd5
Bergulff <Bergulff@hotmail.com>
2024-11-05 11:39:40 +0100

    ship-card

---
+++ src/components/ShipCard.tsx
@@ -0,0 +1,14 @@
+// import refactored data fetch (vessel)
+
+/* function ShipCard({ ship }) {
+    return (
+        <div className="border border-gray-300 p-4 m-2 rounded-lg shadow-md">
+            <h3>{ship.MMSI}</h3>
+            <p>Type: {ship.VesselType}</p>
+            <p>Latitude: {ship.latitude}</p>
+            <p>Longitude: {ship.longitude}</p>
+            <p>COG: {ship.cog}</p>
+            <p>SOG: {ship.sog}</p>
+        </div>
+    );
+} */
