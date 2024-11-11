dde7413ab565d28e0750a98bb7433b54c766e3ee
Mads Risager <madssr2@gmail.com>
2024-11-11 12:36:02 +0100

    Add dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    ssr: true,
+    outDir: "dist",
+    rollupOptions: {
+      input: resolve(__dirname, 'src/main.tsx'),
+    }
+  },
 })
