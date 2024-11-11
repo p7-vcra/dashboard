0a392988ae2587b307b7d7a7faf552a7786e33f1
Mads Risager <madssr2@gmail.com>
2024-11-11 10:18:50 +0100

    Refactor data logic

---
+++ src/types/Vessel.tsx
@@ -0,0 +1,11 @@
+interface Vessel {
+  mmsi: number;
+  vesselType: string;
+  latitude: number;
+  longitude: number;
+  history: Array<{ latitude: number; longitude: number; timestamp: string; cog: number; sog: number }>;
+  cog: number;
+  sog: number;
+}
+
+export { type Vessel };
