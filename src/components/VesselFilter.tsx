53ed07d12c518311a167b6e3c421a36f00830c19
Mads Risager <madssr2@gmail.com>
2024-11-19 11:29:12 +0100

    Fix styles

--- src/components/VesselFilter.tsx
+++ src/components/VesselFilter.tsx
@@ -42,19 +42,20 @@ function VesselFilter({ onClose }: VesselFilterProps) {
 
     const filters = {
         sog: {
-            label: "Speed over ground",
+            display: "Speed over ground",
             min: 0,
             max: 30,
             type: "number",
             step: 1,
         },
         vesselType: {
-            label: "Vessel type",
+            display: "Vessel type",
             options: ["Class A", "Class B", "Base Station", "AtoN"],
             type: "select",
         },
         hasFutureLocation: {
-            label: "Has future location",
+            display: "Trajectory prediction",
+            label: "Only forecasted location",
             type: "checkbox",
         },
     };
@@ -74,11 +75,9 @@ function VesselFilter({ onClose }: VesselFilterProps) {
                 <div className="flex space-x-8 pt-2">
                     {Object.entries(filters).map(([key, value]) => (
                         <div key={key} className="space-y-2 flex flex-col">
-                            <label className="text-white" htmlFor={key}>
-                                {value.label}
-                            </label>
+                            <div className="text-white">{value.display}</div>
                             {value.type === "number" ? (
-                                <div className="space-x-2 flex text-white">
+                                <div className="space-x-2 flex text-white h-full">
                                     <input
                                         type="number"
                                         id={`${key}-range`}
@@ -105,7 +104,7 @@ function VesselFilter({ onClose }: VesselFilterProps) {
                                                 min: Number(e.target.value),
                                             })
                                         }
-                                        className=" bg-zinc-700 border-zinc-600 border-2 rounded-lg p-2 w-24"
+                                        className=" bg-zinc-700 border-zinc-600 border-2 rounded-lg p-2  w-full"
                                     />
                                     <input
                                         type="number"
@@ -133,7 +132,7 @@ function VesselFilter({ onClose }: VesselFilterProps) {
                                                 max: Number(e.target.value),
                                             })
                                         }
-                                        className=" bg-zinc-700 border-zinc-600 border-2 rounded-lg p-2 w-24"
+                                        className=" bg-zinc-700 border-zinc-600 border-2 rounded-lg p-2  w-full"
                                     />
                                 </div>
                             ) : (
@@ -141,7 +140,7 @@ function VesselFilter({ onClose }: VesselFilterProps) {
                                     <select
                                         id={key}
                                         name={key}
-                                        className="bg-zinc-700 border-zinc-600 border-2 rounded-lg p-2"
+                                        className="bg-zinc-700 border-zinc-600 border-2 rounded-lg p-2 h-full"
                                         defaultValue=""
                                         onChange={(e) =>
                                             setVesselType(e.target.value)
@@ -159,28 +158,40 @@ function VesselFilter({ onClose }: VesselFilterProps) {
                                 )
                             )}
                             {value.type === "checkbox" && (
-                                <input
-                                    type="checkbox"
-                                    id={key}
-                                    name={key}
-                                    onChange={(e) =>
-                                        setHasFutureLocation(e.target.checked)
-                                    }
-                                    className="bg-zinc-700 border-zinc-600 border-2  rounded-lg p-2"
-                                />
+                                <div className="peer-container">
+                                    <input
+                                        type="checkbox"
+                                        id={key}
+                                        name={key}
+                                        onChange={(e) =>
+                                            setHasFutureLocation(
+                                                e.target.checked
+                                            )
+                                        }
+                                        className="peer hidden"
+                                    />
+                                    <div className=" bg-zinc-700 border-2 border-zinc-600 rounded-lg flex peer-checked:bg-zinc-500 peer-checked:border-zinc-300">
+                                        <label
+                                            htmlFor={key}
+                                            className="text-white p-4 "
+                                        >
+                                            {"label" in value && value.label}
+                                        </label>
+                                    </div>
+                                </div>
                             )}
                         </div>
                     ))}
-                    <div className="flex items-center flex-col space-y-2 w-44">
+                    <div className="flex items-center justify-between flex-col space-y-2 w-44 text-sm">
                         <button
                             onClick={applyFilter}
-                            className="bg-zinc-600 text-white p-2 rounded-lg hover:bg-zinc-500 active:bg-zinc-700 w-full h-min"
+                            className="bg-zinc-700 text-white p-2 rounded-lg hover:bg-zinc-600 active:bg-zinc-700 w-full border-2 border-zinc-600"
                         >
                             Apply filter
                         </button>
                         <button
                             onClick={clearFilter}
-                            className="bg-zinc-600 text-white p-2 rounded-lg hover:bg-zinc-500 active:bg-zinc-700 w-full h-min"
+                            className="bg-zinc-700 text-white p-2 rounded-lg hover:bg-zinc-600 active:bg-zinc-700 w-full border-2 border-zinc-600"
                         >
                             Clear filter
                         </button>
