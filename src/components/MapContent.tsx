2bf889df922cb09c7ff5c190833fefa3f07b9a4b
Mads Risager <madssr2@gmail.com>
2024-11-20 10:53:11 +0100

    Add coloured markers according to cri

--- src/components/MapContent.tsx
+++ src/components/MapContent.tsx
@@ -1,9 +1,5 @@
-import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import L, { LatLng, MarkerCluster } from "leaflet";
-import React from "react";
-import { renderToStaticMarkup } from "react-dom/server";
-import { Marker, MarkerProps, useMap } from "react-leaflet";
+import L, { MarkerCluster } from "leaflet";
+import { useMap } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import { useActiveVessel } from "../contexts/ActiveVesselContext";
 import { useVesselData } from "../contexts/VesselsContext";
@@ -12,61 +8,12 @@ import { Vessel } from "../types/vessel";
 import "leaflet-rotatedmarker";
 import { Polyline } from "react-leaflet";
 import { useMapOptions } from "../contexts/MapOptionsContext";
-
-const MemoizedMarker = React.memo(
-    function MarkerComponent({
-        position,
-        isActive,
-        rotationAngle,
-        ...props
-    }: MarkerProps & {
-        vessel: Vessel;
-        isActive: boolean;
-        rotationAngle: number;
-    }) {
-        return (
-            <Marker
-                position={position}
-                icon={createVesselIcon(isActive)}
-                //@ts-expect-error rotationAngle is imported from leaflet-rotatedmarker
-                rotationAngle={rotationAngle}
-                rotationOrigin="center center"
-                {...props}
-            />
-        );
-    },
-    function areEqual(prevProps, nextProps) {
-        return (
-            (prevProps.position as LatLng).lat ===
-                (nextProps.position as LatLng).lat &&
-            (prevProps.position as LatLng).lng ===
-                (nextProps.position as LatLng).lng &&
-            prevProps.vessel.mmsi === nextProps.vessel.mmsi &&
-            prevProps.isActive === nextProps.isActive
-        );
-    }
-);
-
-const arrowMarkup = renderToStaticMarkup(
-    <FontAwesomeIcon
-        icon={faLocationArrow}
-        transform={{ rotate: -45, size: 20 }}
-    />
-); // 45 degrees counter clockwise as the icon points NE by default
+import { VesselMarker } from "./VesselMarker";
 
 function createClusterIcon(cluster: MarkerCluster) {
     return L.divIcon({
-        html: `<span class="text-white bg-red-700  h-7 w-7 font-medium rounded-full flex justify-center items-center">${cluster.getChildCount()}</span>`,
+        html: `<span class="text-white bg-zinc-700 border border-zinc-500 h-7 w-7 font-medium rounded-full flex justify-center items-center">${cluster.getChildCount()}</span>`,
         iconSize: L.point(33, 33, true),
-    });
-}
-
-function createVesselIcon(isActive: boolean) {
-    const borderClass = isActive
-        ? "border-blue-600 border-opacity-100"
-        : "border-opacity-0 border-red-600";
-    return L.divIcon({
-        html: `<div class="border-2 m-[-8px] h-7 w-7 flex justify-center items-center hover:border-opacity-100 rounded-full ${borderClass}">${arrowMarkup}</div>`,
     });
 }
 
@@ -96,7 +43,6 @@ function MapContent() {
     });
 
     const { filtered } = useVesselData();
-
     const { activeVessel, setActiveVessel } = useActiveVessel();
 
     return (
@@ -104,20 +50,18 @@ function MapContent() {
         <MarkerClusterGroup
             iconCreateFunction={createClusterIcon}
             animate
-            spiderfyOnMaxZoom
+            chunkedLoading
         >
             {activeVessel && activeVessel.futureLocation && (
                 <Polyline
                     positions={activeVessel.futureLocation}
-                    color="red"
+                    color="#1d4ed8"
                     weight={2}
                 />
             )}
             {Object.values(filtered).map((vessel: Vessel) => (
-                <MemoizedMarker
+                <VesselMarker
                     key={vessel.mmsi}
-                    position={new LatLng(vessel.latitude, vessel.longitude)}
-                    rotationAngle={vessel.cog}
                     vessel={vessel}
                     isActive={activeVessel?.mmsi === vessel.mmsi}
                     eventHandlers={{
---
+++ src/components/VesselMarker.tsx
@@ -0,0 +1,65 @@
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import L, { LatLng } from "leaflet";
+import React from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+import { Marker, MarkerProps } from "react-leaflet";
+import { Vessel } from "../types/vessel";
+
+const VesselMarker = React.memo(
+    function MarkerComponent({
+        vessel,
+        isActive,
+        ...props
+    }: Omit<MarkerProps, "position"> & {
+        vessel: Vessel;
+        isActive: boolean;
+    }) {
+        return (
+            <Marker
+                position={new LatLng(vessel.latitude, vessel.longitude)}
+                icon={createVesselIcon(isActive, vessel.cri)}
+                //@ts-expect-error rotationAngle is imported from leaflet-rotatedmarker
+                rotationAngle={vessel.cog}
+                rotationOrigin="center center"
+                {...props}
+            />
+        );
+    },
+    function areEqual(prevProps, nextProps) {
+        return (
+            prevProps.vessel.latitude === nextProps.vessel.latitude &&
+            prevProps.vessel.longitude === nextProps.vessel.longitude &&
+            prevProps.vessel.mmsi === nextProps.vessel.mmsi &&
+            prevProps.isActive === nextProps.isActive
+        );
+    }
+);
+
+const arrowMarkup = renderToStaticMarkup(
+    <FontAwesomeIcon
+        icon={faLocationArrow}
+        transform={{ rotate: -45, size: 20, y: 2 }}
+    />
+); // 45 degrees counter clockwise as the icon points NE by default
+
+function createVesselIcon(isActive: boolean, cri?: number) {
+    const borderClass = isActive
+        ? "border-blue-700 border-opacity-100"
+        : "border-opacity-0 border-zinc-500";
+
+    const colorClass =
+        cri && cri >= 0.9
+            ? "text-red-600 bg-red-100 bg-opacity-50"
+            : cri && cri >= 0.75
+            ? "text-orange-600 bg-orange-100 bg-opacity-50"
+            : cri && cri >= 0.5
+            ? "text-yellow-600 bg-yellow-100 bg-opacity-50"
+            : "text-zinc-900";
+
+    return L.divIcon({
+        html: `<div class="border-2 m-[-8px] h-7 w-7 flex justify-center items-center hover:border-opacity-100 rounded-full ${borderClass} ${colorClass}">${arrowMarkup}</div>`,
+    });
+}
+
+export { VesselMarker };
