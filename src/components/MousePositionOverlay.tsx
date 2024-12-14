e04b9b928f4d9da327c795e88587d6340366c1c3
Mads Risager <madssr2@gmail.com>
2024-12-14 19:53:51 +0100

    Add scale

--- src/components/MousePositionOverlay.tsx
+++ src/components/MousePositionOverlay.tsx
@@ -7,7 +7,7 @@ interface MousePositionProps {
 
 function MousePositionOverlay({ position }: MousePositionProps) {
     return (
-        <Container className="flex items-center justify-center absolute bottom-1 left-12 p-2 text-xs  m-2 z-[1000]">
+        <Container className="flex items-center justify-center absolute bottom-1 left-12 p-1 text-xs  m-2 z-[1000]">
             <div className="text-white min-w-36 text-center tabular-nums">
                 {position.lat.toFixed(6) || "-"},{" "}
                 {position.lng.toFixed(6) || "-"}
